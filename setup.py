ce758b86c2cb117f1c42050a2f53dc4bdf864b7a
Danny Cosson <cosson@finxpc.com>
2017-11-03 04:21:18 -0700

    bump patch version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='parallel-ci-runner',
-    version='0.1.9',
+    version='0.1.10',
     description='A framework for defining and running parallel CI tests, '
                 'with support for docker-compose.',
     author='Danny Cosson',
